1650e4cb6055d9d95e299266d9b38c56cc7f96fe
rohith <rohithim529@gmail.com>
2023-11-18 09:10:58 +0530

    first

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import React from 'react';
 import Header from './components/Header';  // Adjust the path based on your folder structure
 import Footer from './components/Footer';  // Adjust the path based on your folder structure
 import Body from './components/Body';      // Adjust the path based on your folder structure
+import Pro from './components/Pro';
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <Body />
-      <Footer />
+     <Pro/>
     </div>
   );
 };
